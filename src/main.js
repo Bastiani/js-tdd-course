201b3e8b44d8a6b1ad573777733afdaa3d34cb97
Rafael Bastiani <rafacdb@gmail.com>
2017-11-25 16:56:58 -0200

    erro no run server, Unexpected token: name

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-const a = 3;
-const b = 2;
-const c = 4;
+import react from 'react';
+import reactDOM from 'react-dom';
+import * as R from 'ramda';
 
-console.log(a, b, c);
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr2 = [1, 2, 3, 11, 12, 13, 14, 15, 16];
+
+const arr3 = R.union(arr1, arr2);
+
+console.log(arr3);
